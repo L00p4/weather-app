9aa1fed83e68ddffd4cac121620ac8ff25df2e1b
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2024-02-22 04:06:52 -0300

    user.config

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar({ }: NavbarProps) {
     <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
       <div className='h-[5rem] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto'>
         <p className='flex items-center justify-center gap-2'>
-          <h2 className='text-gray-500 text-3xl'>Weather</h2>
+          <h2 className='text-gray-500 text-3xl'>Weather </h2>
         </p>
       </div>
     </nav>
