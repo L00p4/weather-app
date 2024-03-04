0e4e84e91deb2f81ba651bb5a77ff01e791bdb65
Guilherme Marucchi <guilherme.marucchi@gmail.com>
2024-03-04 08:04:52 -0300

    finishing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,33 +65,78 @@ function Navbar({ location }: NavbarProps) {
     }
   }
 
-  return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-      <div className="h-[5rem] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-        <p className="flex items-center justify-center gap-2">
-          <h2 className="text-gray-500 text-3xl">Weather </h2>
-          <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
-        </p>
+  function handleCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          setLoadingCity(true);
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+          );
+          setTimeout(() => {
+            setLoadingCity(false);
+            setPlace(response.data.name);
+          }, 500);
+        } catch (error) {
+          setLoadingCity(false);
+        }
+      });
+    }
+  }
 
-        <section className="flex gap-2 items-center">
-          <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
-          <MdOutlineLocationOn className="text-3xl" />
-          <p className="text-slate-900/80 text-sm">{location}</p>
-          <div className="relative">
-            <SearchBox
-              value={city}
-              onSubmit={handleOnSubmitSearch}
-              onChange={(e) => {
-                handleInputChange(e.target.value);
-              }}
+  return (
+    <>
+      <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+        <div className="h-[5rem] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+          <p className="flex items-center justify-center gap-2">
+            <h2 className="text-gray-500 text-3xl">Weather </h2>
+            <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
+          </p>
+
+          <section className="flex gap-2 items-center">
+            <MdMyLocation
+              title="Your current location"
+              onClick={handleCurrentLocation}
+              className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer"
             />
-            <SuggestionBox
-              {...{ showSuggestion, suggestion, handleSuggestionClick, error }}
-            />
-          </div>
-        </section>
-      </div>
-    </nav>
+            <MdOutlineLocationOn className="text-3xl" />
+            <p className="text-slate-900/80 text-sm">{location}</p>
+            <div className="relative hidden md:flex">
+              <SearchBox
+                value={city}
+                onSubmit={handleOnSubmitSearch}
+                onChange={(e) => {
+                  handleInputChange(e.target.value);
+                }}
+              />
+              <SuggestionBox
+                {...{
+                  showSuggestion,
+                  suggestion,
+                  handleSuggestionClick,
+                  error,
+                }}
+              />
+            </div>
+          </section>
+        </div>
+      </nav>
+      <section className="flex max-w-7xl px-3 md:hidden">
+        <div className="relative">
+          <SearchBox
+            value={city}
+            onSubmit={handleOnSubmitSearch}
+            onChange={(e) => {
+              handleInputChange(e.target.value);
+            }}
+          />
+          <SuggestionBox
+            {...{ showSuggestion, suggestion, handleSuggestionClick, error }}
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
